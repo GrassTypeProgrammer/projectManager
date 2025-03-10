9d8fa81dddddc5127a9065b38d258985124954bb
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2025-03-10 08:16:36 +0000

    Styling the active link in the navbar

--- project-manager/app/navbar.tsx
+++ project-manager/app/navbar.tsx
@@ -1,4 +1,8 @@
+'use client';
+
+import classNames from 'classnames';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { BsBugFill } from "react-icons/bs";
 
@@ -7,23 +11,30 @@ type LinkData = {
   href: string,
 }
 
-const navbar = () => {
+const Navbar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath)
   const links: LinkData[] = [
     { label: 'Dashboard', href: '/' },
-    { label: 'Issues', href: 'issues' },
+    { label: 'Issues', href: '/issues' },
   ]
 
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-      {/* TODO: Replace with a logo */}
       <Link href='/'>
         <BsBugFill className='h-8 w-8' />
       </Link>
 
       <ul className='flex flex-1 space-x-6 justify-center'>
         {links.map((item: LinkData) => {
+
           return <li key={item.href}>
-            <Link className='text-zinc-500 hover:text-zinc-800 transition-colors' href={item.href}>
+            <Link
+              className={classNames(
+                item.href == currentPath ? 'text-zinc-900' : 'text-zinc-500',
+                'hover:text-zinc-800 transition-colors'
+              )}
+              href={item.href}>
               {item.label}
             </Link>
           </li>
@@ -33,4 +44,4 @@ const navbar = () => {
   )
 }
 
-export default navbar
+export default Navbar
