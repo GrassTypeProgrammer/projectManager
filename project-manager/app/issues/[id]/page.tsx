5ad8aac9917ab2767c42cfcadb2a64a8cc3543b9
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2025-03-27 07:54:20 +0000

    Style the issue detail page

--- project-manager/app/issues/[id]/page.tsx
+++ project-manager/app/issues/[id]/page.tsx
@@ -1,29 +1,34 @@
 import React from 'react'
 import { prisma } from '@/prisma/client'
 import { notFound } from 'next/navigation'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 
 interface Props {
     params: { id: string }
 }
 
 const DetailPage = async ({ params }: Props) => {
-    if(typeof params.id !== 'number')
-        notFound();
-    
     const issue = await prisma.issue.findUnique({
-        where: {id: parseInt(params.id)}
+        where: { id: parseInt(params.id) }
     })
 
-    if(!issue){
+    if (!issue) {
         notFound();
     }
 
     return (
         <div>
-            <p>{issue.title}</p>
-            <p>{issue.description}</p>
-            <p>{issue.status}</p>
-            <p>{issue.createdAt.toDateString()}</p>
+            {/* Flex and Heading are radix components. They're essentially H1 tag (but can be modified) and a flexbox 
+            and they reduce how much tailwind is needed */}
+            <Heading>{issue.title}</Heading>
+            <Flex gap='3' my='2'>
+                <IssueStatusBadge status={issue.status} />
+                <Text>{issue.createdAt.toDateString()}</Text>
+            </Flex>
+            <Card>
+                {issue.status}
+            </Card>
         </div>
     )
 }
