41ba3ae5d91e55af5db97ee93acd78887167b929
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2025-04-01 14:56:27 +0100

    Refactor the IssueDetails page.

---
+++ project-manager/app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,25 @@
+import { IssueStatusBadge } from '@/app/components'
+import { Heading, Flex, Card } from '@radix-ui/themes'
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import {Issue} from '@prisma/client';
+
+const IssueDetails = ({issue}: {issue: Issue}) => {
+    {/* Flex and Heading are radix components. They're essentially H1 tag (but can be modified) and a flexbox 
+            and they reduce how much tailwind is needed */}
+  return (
+    
+            <Heading>{issue.title}</Heading>
+            <Flex gap='3' my='2'>
+                <IssueStatusBadge status={issue.status} />
+                <Text>{issue.createdAt.toDateString()}</Text>
+            </Flex>
+            <Card className='prose' mt='4'>
+                <ReactMarkdown>
+                    {issue.description}
+                </ReactMarkdown>
+            </Card>
+  )
+}
+
+export default IssueDetails
