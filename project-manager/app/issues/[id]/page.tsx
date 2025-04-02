bd593e912fa26c1a194aef960590de80b8b185b5
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2025-04-02 16:36:48 +0100

    Add delete button

--- project-manager/app/issues/[id]/page.tsx
+++ project-manager/app/issues/[id]/page.tsx
@@ -1,11 +1,12 @@
 import { prisma } from '@/prisma/client'
-import { Box, Grid } from '@radix-ui/themes'
+import { Box, Flex, Grid } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import EditIssueButton from './EditIssueButton'
 import IssueDetails from './IssueDetails'
+import DeleteIssueButton from './edit/DeleteIssueButton'
 
 const DetailPage = async ({ params }: { params: Promise<{ id: string }> }) => {
-    const {id} = await params;
+    const { id } = await params;
     const issue = await prisma.issue.findUnique({
         where: { id: parseInt(id) }
     })
@@ -17,13 +18,18 @@ const DetailPage = async ({ params }: { params: Promise<{ id: string }> }) => {
     return (
         // initial is for small devices such as phones and above. md is medium devices, such as tablets and above.
         // So, if it's a medium sized device, 2 columns. Otherwise, 1 column. 
-        <Grid columns={{ initial: '1', md: '2' }} gap='5'>
-            <Box>
+        <Grid columns={{ initial: '1', sm: '5' }} gap='5'>
+            {/* md == tablet in tailwind, sm == tablet in radixUI */}
+            {/* Only span 4 columns if device size == medium (eg: tablet) */}
+            <Box className='md:col-span-4'>
                 <IssueDetails issue={issue} />
             </Box>
 
             <Box>
-                <EditIssueButton issueID={issue.id} />
+                <Flex direction='column' gap='4'>
+                    <EditIssueButton issueID={issue.id} />
+                    <DeleteIssueButton issueID={issue.id} />
+                </Flex>
             </Box>
         </Grid>
     )
