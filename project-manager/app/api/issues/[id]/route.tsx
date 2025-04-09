ded3c33e420eef09ec282634ee1c66732b3e4a10
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2025-04-09 08:34:08 +0100

    Enhance the api to assign issues.

--- project-manager/app/api/issues/[id]/route.tsx
+++ project-manager/app/api/issues/[id]/route.tsx
@@ -1,7 +1,7 @@
 //Put should be for replacing an object, patch should be for updating an object
 
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import { prisma } from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -12,21 +12,31 @@ export async function PATCH(
     request: NextRequest,
     { params }: { params: Promise<{ id: string }> }
 ) {
-    const session= await getServerSession(authOptions);
+    const session = await getServerSession(authOptions);
 
-    if(!session){
+    if (!session) {
         // status 401 == unauthorised
-        return NextResponse.json({}, {status: 401});
+        return NextResponse.json({}, { status: 401 });
     }
-    
+
     const { id } = await params;
     const body = await request.json();
     // Validate
-    const validation = issueSchema.safeParse(body);
+    const validation = patchIssueSchema.safeParse(body);
 
     // return error if invalid
     if (!validation.success) {
         return NextResponse.json(validation.error.format(), { status: 400 });
+    }
+
+    const { assignedToUserID, title, description } = body;
+
+    if (assignedToUserID) {
+        const user = await prisma.user.findUnique({ where: { id: assignedToUserID } })
+
+        if (!user) {
+            return NextResponse.json({ error: 'Invalid User' }, { status: 400 });
+        }
     }
 
     // get issue from database
@@ -45,8 +55,9 @@ export async function PATCH(
         where: { id: issue.id },
         // data to update
         data: {
-            title: body.title,
-            description: body.description,
+            title,
+            description,
+            assignedToUserID,
         }
     });
 
@@ -59,24 +70,24 @@ export async function DELETE(
     request: NextRequest,
     { params }: { params: Promise<{ id: string }> }
 ) {
-    const session= await getServerSession(authOptions);
+    const session = await getServerSession(authOptions);
 
-    if(!session){
+    if (!session) {
         // status 401 == unauthorised
-        return NextResponse.json({}, {status: 401});
+        return NextResponse.json({}, { status: 401 });
     }
 
-    const {id} = await params;
+    const { id } = await params;
     const issue = await prisma.issue.findUnique({
-        where: {id: parseInt(id)}
+        where: { id: parseInt(id) }
     });
 
-    if(!issue){
-        return NextResponse.json({error: 'Invalid Issue'}, {status: 404} ); 
+    if (!issue) {
+        return NextResponse.json({ error: 'Invalid Issue' }, { status: 404 });
     }
 
     await prisma.issue.delete({
-        where: {id: parseInt(id)}
+        where: { id: parseInt(id) }
     });
 
     return NextResponse.json({});
--- project-manager/app/validationSchemas.tsx
+++ project-manager/app/validationSchemas.tsx
@@ -2,5 +2,13 @@ import { z } from "zod";
 
 export const issueSchema = z.object({
     title: z.string().min(1, 'title is required.').max(255),
-    description: z.string().min(1, 'description is required.'),
+    // Always have a max for security reasons. 65535 is the max length of a text field
+    description: z.string().min(1, 'description is required.').max(65535),
+});
+
+export const patchIssueSchema = z.object({
+    title: z.string().min(1, 'title is required.').max(255).optional(),
+    description: z.string().min(1, 'description is required.').max(65535).optional(),
+    // nullable allows you to set this as null, which will allow us to unassign a user
+    assignedToUserID: z.string().min(1, 'AssignedToUserId is required').max(255).optional().nullable(),
 });
