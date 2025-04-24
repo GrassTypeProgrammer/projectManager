f6476e19692a55a1b4803ae63b3ee3be07a7ebfc
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2025-04-24 07:32:47 +0100

    Lock app behind sign in

--- project-manager/middleware.ts
+++ project-manager/middleware.ts
@@ -1,11 +1,36 @@
 // A middleware function directs the user to the login page if they try to access a page that you need to be logged in for
 
 // Middleware function provided by next-auth. 
-export {default} from 'next-auth/middleware';
+import { getToken } from "next-auth/jwt";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+    // A token is used to authenticate an app.
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+    const isAuthenticated = !!token;
+    const isLandingPage = req.nextUrl.pathname === "/";
+    const isAuthRoute = req.nextUrl.pathname.startsWith("/api/auth");
+    // test if path has file extension (.js, .png, etc)
+    const isPublicFile = /\.(.*)$/.test(req.nextUrl.pathname);
+    const isNextStatic = req.nextUrl.pathname.startsWith("/_next");
+
+    if (
+        isAuthenticated ||
+        isLandingPage ||
+        isAuthRoute ||
+        isPublicFile ||
+        isNextStatic
+    ) {
+        return NextResponse.next();
+    }
+
+    // Not authenticated and trying to access a protected route
+    return NextResponse.redirect(new URL("/api/auth/signin", req.url));
+}
 
 export const config = {
-    matcher: [
-        '/issues/new',
-        '/issues/edit/:id+'
-    ]
+    // match all routes except these ones.
+    matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 }
