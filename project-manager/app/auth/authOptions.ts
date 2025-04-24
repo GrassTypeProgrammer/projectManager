afda02e872a9c8411d928d035b3fb7f2774fd9fb
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2025-04-24 07:33:32 +0100

    Add id to session.user.

--- project-manager/app/auth/authOptions.ts
+++ project-manager/app/auth/authOptions.ts
@@ -1,18 +1,27 @@
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import {prisma} from "@/prisma/client"
+import { prisma } from "@/prisma/client"
 import { NextAuthOptions } from "next-auth";
 
-const authOptions:NextAuthOptions = {
+const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })],
-    session:{
+    session: {
         strategy: "jwt"
-    }
+    },
+    callbacks: {
+        session: ({ session, token }) => ({
+          ...session,
+          user: {
+            ...session.user,
+            id: token.sub,
+          },
+        }),
+      },
 }
 
 export default authOptions;
